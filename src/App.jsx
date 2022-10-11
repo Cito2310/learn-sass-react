f1cc72baba16a35970bb75c8b8d6065b5d0987be
Cito2310 <cito2310@gmail.com>
2022-10-11 15:06:08 -0300

    Delete sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { ContainerSections } from "./components/ContainerSections"
-import { Section } from "./components/Section"
-import { SideBar } from "./components/SideBar"
 
 import "./config.scss"
 import "./style/title-section.scss"
@@ -8,15 +6,10 @@ import "./style/title-section.scss"
 export const App = () => {
     return (
         <>
-            <SideBar />
-
-            <div className="content">
                 <div className="title-section">
                     <h1>Aprendiendo<br/>SASS</h1>
                 </div>
-                <ContainerSections />
-            </div>
-        
+                <ContainerSections />    
         </>
 
     )
